0c9f3c1b4d015d99d5c480648c046e2c37eb3f38
MERN Stack Developer <mwakidenice@gmail.com>
2026-02-05 23:43:22 +0300

    Update audio_player_service.dart

--- lib/services/audio_player_service.dart
+++ lib/services/audio_player_service.dart
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2025 Wambugu Kinyua
+Copyright (c) 2026 Kaaria Denis
 Licensed under the Creative Commons Attribution 4.0 International (CC BY 4.0).
 https://creativecommons.org/licenses/by/4.0/
 */
